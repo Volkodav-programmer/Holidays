7ed7a838d8e2cf123f2b2d0ce028a30919f743ad
Volkodav-programmer <Zybenkos2016@gmail.com>
2020-09-14 21:50:00 +0300

    ok

--- 1.py
+++ 1.py
@@ -1,25 +1,29 @@
+#import sys
+#sys.stdin = open('input.txt', "r")
+#sys.stdout = open('output.txt', "w")
+#Coup_one, Coup_two=map(int, input().split())
 Coup_one=int(input())
 Coup_two=int(input())
-Turn1=1
-Turn2=0
-while Coup_one!=0 or Coup_two!=0:
-    if Turn1>Turn2:
+Maksim=1
+Dima=0 #1
+while Coup_one!=0 and Coup_two!=0:
+    if Maksim>Dima:
         if Coup_one==Coup_two:
-            Coup_two-=1
+            Coup_two-=1 #1,
         else:
-            Coup_one+=1
+            Coup_one-=1
             Coup_two-=1
     else:
         if Coup_one==Coup_two:
             Coup_one-=1
         else:
-            Coup_two+=1
-            Coup_one-=1
-    if Turn1>Turn2:
-        Turn2+=1
+            Coup_two-=1 #2
+            Coup_one-=1 #1
+    if Maksim>Dima:
+        Dima+=1
     else:
-        Turn1+=1
+        Maksim+=1
 if Coup_one==0:
-     print('победил 2-ой игрок')
+     print('1')
 elif Coup_two==0:
-     print('победил 1-ый игрок')
+     print('2')
---
+++ Naoborot.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=len(str(a))
+c=int(b)
+d='1'+'0'*(c)
+e=int(d)
+f=10
+while e<f:
+    f*=10
+    t=a/f
+    print(t)
+    
